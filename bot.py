22e2f0d2ffe18f712300f40c7699416cc88458a0
Apollo199 <33178995+Apollo199@users.noreply.github.com>
2017-11-15 23:01:28 +0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,41 @@
+import discord 
+import os
+import sys
+import io
+from import.ext import commands
+
+# This part defines the bot prefix, the description and the owner ID.
+
+bot = commands.Bot(command_prefix="-", description="ApolloBot0.5 is a testing bot. This isn't the final product!", owner_id=362615327441420289)
+bot.remove_command('help')
+
+# This part checks the stuff when bot starts
+
+
+@bot.event
+async def on_ready():
+    print("Bot Is Online")
+    
+
+# Help command
+
+@bot.command()
+async def help(ctx):
+    em = discord.Embed(color=discord.Color(value=0xFBCC16)) 
+    em.title = "Help"
+    em.description = "A testing bot"
+    em.add_filed(name="Ping", value="Pong! Isn't it nice?!")
+        await ctx.send(embed=em)
+        
+ 
+ # Ping command
+ 
+ @bot.command()
+ async def ping(ctx):
+     em = discord.Embed(color=discord.Color(value=0xFBCC16))
+     em.title = "Pong!"
+     em.description = await ctx.send("Pong!")
+        await ctx.send(embed=em)
+        
+        
+     
