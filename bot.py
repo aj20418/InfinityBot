d23ebb0cfad7738f1652023c0cc186cd84efe6b2
Apollo199 <33178995+Apollo199@users.noreply.github.com>
2017-11-15 23:44:46 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ async def help(ctx):
     em.description = "A testing bot"
     em.add_filed(name="Ping", value="Pong! Isn't it nice?!")
     em.add_filed(name="Invite", value="Invite ApolloBot to your great server!")
-        await ctx.send(embed=em)
+    await ctx.send(embed=em)
         
  
  # Ping command
@@ -36,7 +36,7 @@ async def help(ctx):
      em = discord.Embed(color=discord.Color(value=0xFBCC16))
      em.title = "Pong!"
      em.description = await ctx.send("Pong!")
-        await ctx.send(embed=em)
+     await ctx.send(embed=em)
         
         
      
@@ -46,7 +46,6 @@ async def help(ctx):
 async def invite(ctx):
     em = discord.Embed(color=discord.Color(value=0xFBCC16))
     em.title = "Invite"
-    em.add_field(name="invite link", value=[Click Here!](https://discordapp.com/oauth2/authorize?client_id=380080034586820618&scope=bot&permissions=1)
-        await ctx.send(embed=em)
-
+    em.add_field(name="invite link", value="[Click Here!](https://discordapp.com/oauth2/authorize?client_id=380080034586820618&scope=bot&permissions=1)")
+    await ctx.send(embed=em)
                  
