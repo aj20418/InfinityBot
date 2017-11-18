ca7a3ae17b2c89aef2564414906629b67e1b1c6a
Apollo199 <33178995+Apollo199@users.noreply.github.com>
2017-11-19 00:05:18 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -70,56 +70,7 @@ async def say(ctx, *, message:str):
     await ctx.send(embed=em
                    
                    
-                   
-@bot.command(hidden=True, name='eval')
-@commands.is_owner()
-async def _eval(ctx, *, body: str):
-        """Evaluates a code"""
-
-        env = {
-            'bot': bot,
-            'ctx': ctx,
-            'channel': ctx.channel,
-            'author': ctx.author,
-            'guild': ctx.guild,
-            'message': ctx.message,
-        }
-
-        env.update(globals())
-
-        body = cleanup_code(body)
-        stdout = io.StringIO()
-
-        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
-
-        try:
-            exec(to_compile, env)
-        except Exception as e:
-            return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
-
-        func = env['func']
-        try:
-            with redirect_stdout(stdout):
-                ret = await func()
-        except Exception as e:
-            value = stdout.getvalue()
-            await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
-        else:
-            value = stdout.getvalue()
-            try:
-                await ctx.message.add_reaction('\u2705')
-            except:
-                pass
-
-            if ret is None:
-                if value:
-                    await ctx.send(f'```py\n{value}\n```')
-            else:
-                await ctx.send(f'```py\n{value}{ret}\n```')
-
-
-                   
-
+                 
                   
                    
         
